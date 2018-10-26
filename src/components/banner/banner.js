251b478459d8b9acf042721949362113acf38750
Said <mokhammiedsaid@gmail.com>
2018-10-26 21:05:41 +0300

    start

---
+++ src/components/banner/banner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Banner extends Component {
+  render() {
+    return (
+      <div className="banner">
+        <div>
+
+        </div>
+      </div>
+    )
+  }
+}
